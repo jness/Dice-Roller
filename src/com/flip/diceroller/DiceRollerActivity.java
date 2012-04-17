42954cc2ce99ba1426905d363be1f785677892c8
Jeffrey Ness <jeffrey.ness@rackspace.com>
2012-04-17 16:08:38 -0500

    latest

--- src/com/flip/diceroller/DiceRollerActivity.java
+++ src/com/flip/diceroller/DiceRollerActivity.java
@@ -1,19 +1,20 @@
 package com.flip.diceroller;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.EditText;
 import android.view.View.OnClickListener;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import com.flip.diceroller.DiceUtils;
 
 
 public class DiceRollerActivity extends Activity implements OnClickListener {
-	
-	private Button d4, d6, d8, d10, d12, d20, d100, coin, reset;
+	private Button d4, d6, d8, d10, d12, d20, d100, coin, roll;
 	private EditText text;
 	private TextView value, label;
 	private DiceUtils dicetools;
@@ -21,6 +22,8 @@ public class DiceRollerActivity extends Activity implements OnClickListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // handle the correct theme for 2.1 and ICS devices
         setContentView(R.layout.main);
         
         // link to each of our textviews
@@ -39,7 +42,7 @@ public class DiceRollerActivity extends Activity implements OnClickListener {
         this.d20 = (Button) findViewById(R.id.d20);
         this.d100 = (Button) findViewById(R.id.d100);
         this.coin = (Button) findViewById(R.id.coin);
-        this.reset = (Button) findViewById(R.id.reset);
+        this.roll = (Button) findViewById(R.id.roll);
         
         // set the listeners
         this.d4.setOnClickListener(this);
@@ -49,28 +52,25 @@ public class DiceRollerActivity extends Activity implements OnClickListener {
         this.d12.setOnClickListener(this);
         this.d20.setOnClickListener(this);
         this.d100.setOnClickListener(this);
-        this.reset.setOnClickListener(this);
+        this.roll.setOnClickListener(this);
         this.coin.setOnClickListener(this);
-        this.text.setOnClickListener(this);
          
     }
         
     public void onClick(View v) {
     	// handle user input
-    	if (this.text.getId() == v.getId()) {
+    	if (this.roll.getId() == v.getId()) {
     		if (this.text.getText().length() != 0) {
     			String input = this.text.getText().toString();
         		this.dicetools = new DiceUtils();
         		this.dicetools.rollDice(input);
         		label.setText(this.dicetools.getLabel());
         		value.setText(this.dicetools.getValue());
-    			this.text.setText("");
+        		
+        		// hide the keyboard
+        		InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        		imm.hideSoftInputFromWindow(this.text.getWindowToken(), 0);
     		}
-    			
-    	// handle reset button
-    	} else if (this.reset.getId() == v.getId()) {
-    		label.setText("");
-    		value.setText("");
     	
     	// handle flip coin
     	} else if (this.coin.getId() == v.getId()) {
